fb0e6b1ac32d20280ff1d53f96809490b9595b16
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-10 01:36:51 +0530

    header component created...used material ui icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a fb clone</h1>
-      {/* header*/}
+      <Header/>
       {/* body*/}
           {/* Sidebar */}
           {/* Feed */}
