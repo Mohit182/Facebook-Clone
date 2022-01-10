9518bb2273b1be10b7863a205b5795795ed28225
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-10 20:06:42 +0530

    google login authenticator added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Feed from "./components/Body/Feed/Feed";
 import Sidebar from "./components/Body/Sidebar/Sidebar";
 import Contacts from "./components/Body/Contacts/Contacts";
 import Header from "./components/Header/Header";
+import Login from "./Login/Login";
 
 function App() {
+  const user = null;
   return (
     <div className="App">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Contacts />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Contacts />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,22 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCPPDaiTEohe7eKKAbq3DHn8K56SjcylbQ",
-    authDomain: "facebook-clone-cf14e.firebaseapp.com",
-    projectId: "facebook-clone-cf14e",
-    storageBucket: "facebook-clone-cf14e.appspot.com",
-    messagingSenderId: "663591297841",
-    appId: "1:663591297841:web:d8cfd2ddc706ef0a0a8610",
-    measurementId: "G-1KYJLFKCPJ"
-  };
+  apiKey: "AIzaSyCPPDaiTEohe7eKKAbq3DHn8K56SjcylbQ",
+  authDomain: "facebook-clone-cf14e.firebaseapp.com",
+  projectId: "facebook-clone-cf14e",
+  storageBucket: "facebook-clone-cf14e.appspot.com",
+  messagingSenderId: "663591297841",
+  appId: "1:663591297841:web:d8cfd2ddc706ef0a0a8610",
+  measurementId: "G-1KYJLFKCPJ",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
