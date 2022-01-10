f86bf4fc3f68815eee1c478f997f560d13c74589
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-10 12:08:10 +0530

    Sidebar component Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Feed from "./components/Body/Feed/Feed";
 import Sidebar from "./components/Body/Sidebar/Sidebar";
 import Header from "./components/Header/Header";
 
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </div>
     </div>
--- src/components/Body/Sidebar/Sidebar.js
+++ src/components/Body/Sidebar/Sidebar.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreOutLined from "@material-ui/icons/ExpandMoreOutlined";
+
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow title='Pages' />
-      <SidebarRow title='Friends' />
-      <SidebarRow title='Pages'  />
-      <SidebarRow />
+      <SidebarRow
+        src="https://avatars.githubusercontent.com/u/56998922?v=4"
+        title="Mohit Gupta"
+      />
+      <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Informatin Center" />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messanger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutLined} title="Marketplace" />
+
       <SidebarRow />
       <SidebarRow />
     </div>
