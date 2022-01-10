43f0c19047465e1ff60f2c2bd3214f736cacfa80
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-10 11:34:16 +0530

    sidebar component making

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Body/Sidebar/Sidebar";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      {/* body*/}
-          {/* Sidebar */}
-          {/* Feed */}
-          {/* Widgets */}
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
