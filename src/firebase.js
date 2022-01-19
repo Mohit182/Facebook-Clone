fd0297f8be71676b218f1cb5b41d0f2d854c4e11
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-19 14:52:28 +0530

    firebase commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import "firebase/compat/firestore";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_WEATHER_API_KEY,
+  apiKey: "AIzaSyCPPDaiTEohe7eKKAbq3DHn8K56SjcylbQ",
   authDomain: "facebook-clone-cf14e.firebaseapp.com",
   projectId: "facebook-clone-cf14e",
   storageBucket: "facebook-clone-cf14e.appspot.com",
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: "G-1KYJLFKCPJ",
+  messagingSenderId: "663591297841",
+  appId: "1:663591297841:web:d8cfd2ddc706ef0a0a8610",
+  measurementId: "G-1KYJLFKCPJ"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 // console.log(process.env.REACT_APP_NOT_SECRET_CODE)
